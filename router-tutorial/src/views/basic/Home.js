ac96da6a4504c3be08f06864172038e4d4ec5154
lisa.dewaele <lisa.dewaele@student.howest.be>
2021-02-05 15:19:51 +0100

    Add Link and NavLink components

--- router-tutorial/src/views/basic/Home.js
+++ router-tutorial/src/views/basic/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useHistory } from "react-router-dom";
+import { useHistory, Link, NavLink } from "react-router-dom";
 
 export default  function Home(){
 
@@ -11,8 +11,34 @@ export default  function Home(){
 
     return(
         <main>
-        <h1>Home</h1>
-        <p onClick={goToAbout}>Click me to go the about!</p>
+            <h1>Home</h1>
+            <h2>Programmatically link</h2>
+            <p onClick={goToAbout}>Click me to go the about!</p>
+
+            <h2>Link components</h2>
+            <h3>Link</h3>
+            <Link to="/users">String Link: Users</Link>
+            <br/>
+            <Link to="/params/1/hello">Link: params</Link>
+            <br/>
+            <Link to= {{ 
+                pathname: "/queries",
+                search: "?name=Lisa&age=20",
+                hash: "#hash",
+             }}>Object Link: Queries</Link>
+            <br/>
+            <Link to={location => ({...location, pathname: "/about"})}>Function Link: about</Link>
+            <br/>
+            <Link to="/users" replace>Replace history entry</Link>
+
+            <h3>NavLink</h3>
+            <NavLink to="/users/create">NavLink: Create user</NavLink>
+            <br/>
+            <NavLink to="/" activeClassName="active">Current page: activeClassName</NavLink>
+            <br/>
+            <NavLink to="/" activeStyle={{ fontWeight: "bold", color: "#00ffbb" }}>Current page: activeStyle</NavLink>
+            
+            
         </main>
 
     )
